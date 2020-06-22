9379ae286d8c2cf098fd848f41656b588729c90a
budisetiawan2207 <budi.2207@students.amikom.ac.id>
2020-06-22 20:59:15 +0700

    commit

---
+++ ClassInduk/Karyawan.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classanak
+{
+    public abstract class Karyawan
+    {
+        public String Nik { get; set; }
+        public String Nama { get; set; }
+        public abstract double Gaji();
+    }
+}
